b66499db81b56333a7dae94a31f7eb7ff65509d1
joaovictor3g <jvdias79797@gmail.com>
2022-10-27 13:42:02 -0300

    fix: adds

--- atividade06/script.js
+++ atividade06/script.js
@@ -75,7 +75,7 @@ createNewPostForm.addEventListener("submit", (event) => {
   const name = formData.get("name");
   const message = formData.get("message");
 
-  posts.push({ name, message });
+  posts.unshift({ name, message });
   renderPosts(posts);
 
   document
@@ -86,3 +86,76 @@ createNewPostForm.addEventListener("submit", (event) => {
 });
 
 renderPosts(posts);
+
+// Handle Gallery image
+
+let currentSlide = 0;
+const imagesLength = 10;
+
+const galleryImageModal = document.querySelector(
+  ".overlay-image-gallery--modal .slides .images-slider"
+);
+const next = document.querySelector("button.next");
+const prev = document.querySelector("button.prev");
+
+next.addEventListener("click", () => {
+  if (currentSlide === imagesLength) {
+    next.setAttribute("disabled", true);
+  } else {
+    prev.removeAttribute("disabled");
+    currentSlide++;
+  }
+  renderImages();
+});
+
+prev.addEventListener("click", () => {
+  if (currentSlide <= 0) {
+    prev.setAttribute("disabled", true);
+  } else {
+    next.removeAttribute("disabled");
+    currentSlide--;
+  }
+  renderImages();
+});
+
+function renderImages() {
+  if (currentSlide <= 0) {
+    prev.setAttribute("disabled", true);
+  }
+
+  const images = Array.from({ length: imagesLength });
+
+  images.forEach((_, i) => {
+    const img = document.createElement("img");
+    img.src = `https://picsum.photos/900/600?random=${i}`;
+    galleryImageModal.appendChild(img);
+  });
+
+  galleryImageModal.childNodes.forEach((child) =>
+    child.classList.remove("active")
+  );
+
+  galleryImageModal.childNodes[currentSlide].classList.add("active");
+}
+
+renderImages();
+
+const overlayGalleryImage = document.querySelector(".overlay-image-gallery");
+const closeGalleryImageModalButton = document.querySelector(
+  ".overlay-image-gallery .overlay-image-gallery--modal .close-modal"
+);
+const openGalleryImageModalButton = document.querySelector(
+  "button.image-gallery"
+);
+
+closeGalleryImageModalButton.addEventListener("click", closeGalleryImageModal);
+
+openGalleryImageModalButton.addEventListener("click", openGalleryImageModal);
+
+function closeGalleryImageModal() {
+  overlayGalleryImage.classList.remove("opened");
+}
+
+function openGalleryImageModal() {
+  overlayGalleryImage.classList.add("opened");
+}
